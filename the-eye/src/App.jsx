3dd81baf754b0a1dc6fc4695129ea09a7286d1c8
SHALMAN <shalman4502n@gmail.com>
2025-08-04 23:51:05 +0530

    hero section

--- the-eye/src/App.jsx
+++ the-eye/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import './components/HomePage'
@@ -12,6 +11,7 @@ function App() {
   return (
     <>
    <HomePage></HomePage>
+  
     </>
   )
 }
